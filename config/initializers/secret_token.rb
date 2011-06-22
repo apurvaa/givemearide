8bfeef0c1e00265e3d60e3e3567fa9f8c99985fe
Apurva Agrawal <apurva.agrawal@gmail.com>
2011-06-22 14:02:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Givemearide::Application.config.secret_token = '2dff8c8d32eccbdbb041af0ac0b0f48b635ddc9c543e7cb46c72242884cf375716e88e8b1d645b354ff613c62c0aa2695a6410bf1d804f9e6d4807ef8999878d'
